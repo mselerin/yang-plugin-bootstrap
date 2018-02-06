b8e33b798ba8f45f371de65b7e626ae56db6ba4d
Michel Selerin <michel.selerin@outlook.com>
2018-02-06 11:09:16 +0100

    feat: Update bootstrap version

--- src/generators/bootstrap/yang-bootstrap.generator.ts
+++ src/generators/bootstrap/yang-bootstrap.generator.ts
@@ -1,21 +1,42 @@
-import {YangGenerator} from "yang-cli/dist/generators/yang.generator";
-import {YangUtils} from "yang-cli/dist/helpers/yang-utils";
-import extend = require('deep-extend');
+import * as path from 'path';
+import * as extend from "deep-extend";
+import {YangGenerator} from "yang-cli/dist/src/generators/yang.generator";
+import {FileUtils} from "yang-cli/dist/src/helpers/file-utils";
 
 export class YangBootstrapGenerator extends YangGenerator
 {
-    async _writing(): Promise<void> {
-        await super._writing();
+    async runWriting(): Promise<void> {
+        await super.runWriting();
+        await this.copyTemplates();
+        this.updatePackageJson();
 
-        // Copy templates
-        this._copyTemplates();
+        // MAJ de styles.scss avec l'import de bootstrap
+        {
+            let file = `${this.projectRoot}src/styles.scss`;
+            let content = FileUtils.read(file);
+            content = `@import "assets/styles/bootstrap-custom";\r\n${content}`;
+            FileUtils.write(file, content);
+        }
 
-        // MAJ de package.json
-        const pkg = this.fs.readJSON(this.destinationPath(`${this.projectRoot}/package.json`), {});
+
+        // MAJ de _variable.scss
+        {
+            let file = `${this.projectRoot}src/assets/styles/_variables.scss`;
+            let content = FileUtils.read(file);
+            content = `@import "~bootstrap/scss/functions";\r\n\r\n${content}\r\n\r\n@import "~bootstrap/scss/variables";`;
+            FileUtils.write(file, content);
+        }
+    }
+
+
+
+    updatePackageJson(): void {
+        const file = path.join(this.root, 'package.json');
+        const pkg = FileUtils.readJSON(file);
 
         extend(pkg, {
             dependencies: {
-                "bootstrap": "4.0.0-beta",
+                "bootstrap": "4.0.0",
                 "jquery": "3.2.1"
             },
             devDependencies: {
@@ -23,44 +44,6 @@ export class YangBootstrapGenerator extends YangGenerator
             }
         });
 
-        this.fs.writeJSON(this.destinationPath(`${this.projectRoot}/package.json`), pkg);
-
-
-        // MAJ de styles.scss avec l'import de bootstrap
-        {
-            let file = `${this.projectRoot}src/styles.scss`;
-            let content = this.fs.read(this.destinationPath(file));
-            content = `@import "assets/styles/bootstrap-custom";\r\n${content}`;
-            this.fs.write(file, content);
-        }
-
-
-        // MAJ de _variable.scss
-        {
-            let file = `${this.projectRoot}src/assets/styles/_variables.scss`;
-            let content = this.fs.read(this.destinationPath(file));
-            content = `@import "~bootstrap/scss/functions";\r\n\r\n${content}\r\n\r\n@import "~bootstrap/scss/variables";`;
-            this.fs.write(file, content);
-        }
-
-
-        // MAJ de main.ts avec l'import de bootstrap
-        // {
-        //     let file = `${this.projectRoot}src/main.ts`;
-        //     let content = this.fs.read(this.destinationPath(file));
-        //     content = `import 'bootstrap';\r\n${content}`;
-        //     this.fs.write(file, content);
-        // }
+        FileUtils.writeJSON(file, pkg);
     }
-
-
-
-    // Declaration du runLoop yeoman
-    initializing() { return super.initializing(); }
-    prompting() { return super.prompting(); }
-    configuring() { return super.configuring(); }
-    writing() { return super.writing(); }
-    conflicts() { return super.conflicts(); }
-    install() { return super.install(); }
-    end() { return super.end(); }
 }
